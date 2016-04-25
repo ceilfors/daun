addb2e0c4d5105d51edff4f5a4030e8b8153c877
Wisen Tanasa <wisen@ceilfors.com>
2016-04-25 13:19:36 +0100

    Make spec DRY.

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -14,13 +14,7 @@ describe 'cli' do
     Dir.mktmpdir do |dir|
       # Preparation
       bare_repository = File.join(dir, 'bare-repository')
-      Git.init(bare_repository, :bare => true)
-      working_tree_path = File.join(dir, 'working_tree')
-      working_tree_repo = Git.clone(bare_repository, working_tree_path)
-      FileUtils.cp_r 'spec/repo/.', working_tree_path
-      working_tree_repo.add(:all => true)
-      working_tree_repo.commit('Add repo')
-      working_tree_repo.push
+      create_bare_repository bare_repository
 
       # Execute
       destination = File.join(dir, 'repository')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'git-opengrok'
+
+def create_bare_repository(dir)
+  bare_repository = dir
+  Git.init(bare_repository, :bare => true)
+  working_tree_path = File.join(dir, 'working_tree')
+  working_tree_repo = Git.clone(bare_repository, working_tree_path)
+  FileUtils.cp_r 'spec/repo/.', working_tree_path
+  working_tree_repo.add(:all => true)
+  working_tree_repo.commit('Add repo')
+  working_tree_repo.push
+end
