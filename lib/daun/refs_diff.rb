67fed8f5834b80b461655c01b867a9ffb6a284ca
Wisen Tanasa <wisen@ceilfors.com>
2016-05-30 00:13:56 +0100

    Filters updated references.

--- lib/daun/refs_diff.rb
+++ lib/daun/refs_diff.rb
@@ -12,10 +12,11 @@ class RefsDiff
     type ? keys.select { |k| k.start_with? "refs/#{type}" } : keys
   end
 
-  def updated
-    (@after.keys + @before.keys)
+  def updated(type = nil)
+    keys = (@after.keys + @before.keys)
         .group_by { |k| k }
         .select { |k, k_group| k_group.size > 1 && @before[k] != @after[k] }
         .collect { |k, k_group| k.to_s }
+    type ? keys.select { |k| k.start_with? "refs/#{type}" } : keys
   end
 end
--- spec/refs_diff_spec.rb
+++ spec/refs_diff_spec.rb
@@ -47,7 +47,7 @@ describe 'refs_diff' do
           :expected => ['refs/remotes/origin/master']
       }
   ].each do |example|
-    it 'filters references by type' do
+    it 'filters added references by type' do
       ref_diff = RefsDiff.new(example[:before], example[:after])
 
       expect(ref_diff.added example[:type]).to match_array(example[:expected])
@@ -55,6 +55,11 @@ describe 'refs_diff' do
   end
 
   [
+      {
+          :before => {},
+          :after => {:'refs/remotes/origin/master' => '1'},
+          :expected => []
+      },
       {
           :before => {:'refs/remotes/origin/master' => '1'},
           :after => {:'refs/remotes/origin/master' => '1'},
@@ -74,6 +79,27 @@ describe 'refs_diff' do
       ref_diff = RefsDiff.new(example[:before], example[:after])
 
       expect(ref_diff.updated).to match_array(example[:expected])
+    end
+  end
+
+
+  [
+      {
+          :before => {:'refs/tags/1.0' => '1', :'refs/remotes/origin/master' => '1'},
+          :after => {:'refs/tags/1.0' => '2', :'refs/remotes/origin/master' => '2'},
+          :type => :tags,
+          :expected => ['refs/tags/1.0']
+      }, {
+          :before => {:'refs/tags/1.0' => '1', :'refs/remotes/origin/master' => '1'},
+          :after => {:'refs/tags/1.0' => '1', :'refs/remotes/origin/master' => '2'},
+          :type => :remotes,
+          :expected => ['refs/remotes/origin/master']
+      }
+  ].each do |example|
+    it 'filters updated references by type' do
+      ref_diff = RefsDiff.new(example[:before], example[:after])
+
+      expect(ref_diff.updated example[:type]).to match_array(example[:expected])
     end
   end
 
