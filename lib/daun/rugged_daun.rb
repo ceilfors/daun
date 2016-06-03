40afbcf486fa7a6fc1b76e73c1eaa9cea7c90460
Wisen Tanasa <wisen@ceilfors.com>
2016-06-03 07:18:20 +0100

    Use refs_diff in daun implementation.

--- lib/daun/rugged_daun.rb
+++ lib/daun/rugged_daun.rb
@@ -1,5 +1,6 @@
 require 'rugged'
 require 'fileutils'
+require 'daun/refs_diff'
 
 class RuggedDaun
 
@@ -12,23 +13,40 @@ class RuggedDaun
   end
 
   def checkout
-    fetch_result = fetch
+    # fetch_result = fetch
+    refs_diff = get_refs_diff
 
-    # Updates all branches
-    @repository.branches.each_name(:remote) do |branch|
+    refs_diff.added(:remotes).each do |refs|
+      branch = @repository.branches[refs[/refs\/remotes\/(.*)/, 1]].name
       local_branch_name = branch[/origin\/(.*)/, 1]
       checkout_target_directory = File.join(@repository.workdir, "branches", local_branch_name)
       FileUtils::mkdir_p checkout_target_directory
       @repository.checkout(branch, strategy: :force, target_directory: checkout_target_directory)
     end
 
-    fetch_result.deleted_branches.each do |branch|
-      FileUtils.rm_rf File.join(@repository.workdir, 'branches', branch)
+    refs_diff.updated(:remotes).each do |refs|
+      branch = @repository.branches[refs[/refs\/remotes\/(.*)/, 1]].name
+      local_branch_name = branch[/origin\/(.*)/, 1]
+      checkout_target_directory = File.join(@repository.workdir, "branches", local_branch_name)
+      FileUtils::mkdir_p checkout_target_directory
+      @repository.checkout(branch, strategy: :force, target_directory: checkout_target_directory)
     end
 
-    @repository.tags.each do |tag|
+    refs_diff.deleted(:remotes).each do |refs|
+      local_branch_name = refs[/refs\/remotes\/origin\/(.*)/, 1]
+      FileUtils.rm_rf File.join(@repository.workdir, 'branches', local_branch_name)
+    end
+
+    refs_diff.added(:tags).each do |refs|
+      tag = @repository.tags[refs[/refs\/tags\/(.*)/, 1]]
       checkout_target_directory = File.join(@repository.workdir, "tags", tag.name)
-      # TODO Check if git reference difference before deleting
+      FileUtils::mkdir_p checkout_target_directory
+      @repository.checkout(tag.target.oid, strategy: :force, target_directory: checkout_target_directory)
+    end
+
+    refs_diff.updated(:tags).each do |refs|
+      tag = @repository.tags[refs[/refs\/tags\/(.*)/, 1]]
+      checkout_target_directory = File.join(@repository.workdir, "tags", tag.name)
       if File.exists? checkout_target_directory
         # checkout --force is somehow not working to update the tag
         FileUtils.rm_rf checkout_target_directory
@@ -37,39 +55,24 @@ class RuggedDaun
       @repository.checkout(tag.target.oid, strategy: :force, target_directory: checkout_target_directory)
     end
 
-    fetch_result.deleted_tags.each do |tag|
-      FileUtils.rm_rf File.join(@repository.workdir, 'tags', tag)
+    refs_diff.deleted(:tags).each do |refs|
+      tag_name = refs[/refs\/tags\/(.*)/, 1]
+      FileUtils.rm_rf File.join(@repository.workdir, 'tags', tag_name)
     end
   end
 
   private
 
-  def fetch
-    before_fetch_branches = @repository.branches.each_name.to_a.collect { |branch| branch[/origin\/(.*)/, 1]}
+  def get_refs_diff
+    before_fetch = Hash[@repository.refs.collect { |r| [ r.canonical_name, r.target_id ] } ]
+
     delete_all_remote_branches # Prune is not supported by rugged! Deleting all remote refs and re-fetch
-    before_fetch_tags = @repository.tags.each_name.to_a
     delete_all_tags
-
-    fetch_result = FetchResult.new
     @repository.remotes['origin'].fetch
-    after_fetch_branches = @repository.branches.each_name.to_a.collect { |branch| branch[/origin\/(.*)/, 1]}
-    after_fetch_tags = @repository.tags.each_name.to_a
 
-    fetch_result.new_branches = after_fetch_branches - before_fetch_branches
-    fetch_result.deleted_branches = before_fetch_branches - after_fetch_branches
-    fetch_result.existing_branches = before_fetch_branches - fetch_result.new_branches
+    after_fetch = Hash[@repository.refs.collect { |r| [ r.canonical_name, r.target_id ] } ]
 
-    fetch_result.new_tags = after_fetch_tags - before_fetch_tags
-    fetch_result.deleted_tags = before_fetch_tags - after_fetch_tags
-    fetch_result.existing_tags = before_fetch_tags - fetch_result.new_tags
-
-    fetch_result
-  end
-
-  class FetchResult
-
-    attr_accessor :new_branches, :deleted_branches, :existing_branches,
-                  :new_tags, :deleted_tags, :existing_tags
+    RefsDiff.new(before_fetch, after_fetch)
   end
 
   def delete_all_remote_branches
