93fd5bb94fc990e4938de0e7751ec43f3de3bc04
Wisen Tanasa <wisen@ceilfors.com>
2016-07-10 13:39:33 +0100

    Rubocop cli.rb.

--- lib/daun/cli.rb
+++ lib/daun/cli.rb
@@ -1,16 +1,20 @@
 require 'thor'
 require 'daun/rugged_daun'
 
-class Daun::CLI < Thor
+module Daun
+  # Daun CLI. All commands are made available by this class.
+  class CLI < Thor
+    desc 'init remote_url destination', 'Initialize a daun directory'
 
-  desc "init remote_url destination", "Initialize a daun directory"
-  def init(remote_url, destination)
-    RuggedDaun.new(destination).init(remote_url)
-  end
+    def init(remote_url, destination)
+      RuggedDaun.new(destination).init(remote_url)
+    end
 
-  desc "checkout", "Checks out git working tree as per daun configuration"
-  option :directory
-  def checkout()
-    RuggedDaun.new(options[:directory]).checkout
+    desc 'checkout', 'Checks out git working tree as per daun configuration'
+    option :directory
+
+    def checkout
+      RuggedDaun.new(options[:directory]).checkout
+    end
   end
 end
