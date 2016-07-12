9bf56c36e4eca0ffa9067fd79c11ed70e96209d2
Wisen Tanasa <wisen@ceilfors.com>
2016-07-12 07:22:56 +0100

    Fail rspec if code coverage drops below 90%.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
-
+SimpleCov.minimum_coverage 90
 SimpleCov.start do
   formatter SimpleCov::Formatter::MultiFormatter.new(
     [
