61afdb16784cfd03c1a126a55927e76cdbeb48e2
Wisen Tanasa <wisen@ceilfors.com>
2016-07-17 09:09:52 +0100

    Simplify logging pattern.

--- lib/daun.rb
+++ lib/daun.rb
@@ -3,5 +3,5 @@ require 'daun/version'
 require 'daun/rugged_daun'
 require 'daun/refs_diff'
 
-Logging.logger.root.appenders = Logging.appenders.stdout
+Logging.logger.root.appenders = Logging.appenders.stdout(layout: Logging.layouts.pattern(pattern: "%m\n"))
 Logging.logger.root.level = :info
