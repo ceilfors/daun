8bc34586f35c5c0ce75fd3f8ebeec683df8106a5
Wisen Tanasa <wisen@ceilfors.com>
2016-07-24 08:53:44 +0100

    Add yardoc to refs_diff.

--- lib/daun/refs_diff.rb
+++ lib/daun/refs_diff.rb
@@ -4,16 +4,29 @@ module Daun
   class RefsDiff
     attr_accessor :added_remotes
 
+    # Creates a new instance of `RefsDiff`.
+    #
+    # @param before [Hash] the refs hash with full refs format as key and commit id as value
+    # @param after [Hash] the refs hash with full refs format as key and commit id as value
     def initialize(before, after)
       @before = before
       @after = after
     end
 
+    # Returns all of the refs that have been added after the fetch. These are the refs
+    # that exists in `after` but not `before`.
+    #
+    # @param type [Symbol] :tag for tag refs, :remotes for remote branches, nil for everything
     def added(type = nil)
       keys = (@after.keys - @before.keys).collect(&:to_s)
       filter(keys, type)
     end
 
+    # Returns all of the refs that have been updated after the fetch. Updated refs
+    # are detected when refs exists in both `before` and `after` but is having a
+    # different commit id in the `Hash`.
+    #
+    # @param type [Symbol] :tag for tag refs, :remotes for remote branches, nil for everything
     def updated(type = nil)
       keys = (@after.keys + @before.keys)
                  .group_by { |k| k }
@@ -22,6 +35,10 @@ module Daun
       filter(keys, type)
     end
 
+    # Returns all of the refs that have been deleted after the fetch. These are the
+    # refs that exists in `before` but not `after`
+    #
+    # @param type [Symbol] :tag for tag refs, :remotes for remote branches, nil for everything
     def deleted(type = nil)
       keys = (@before.keys - @after.keys).collect(&:to_s)
       filter(keys, type)
